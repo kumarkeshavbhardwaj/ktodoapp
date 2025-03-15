4f48f90818ddfc2e749694baed7c7ede43dd134b
kumar <kumarkeshavbhardwaj@gmail.com>
2025-03-15 22:45:55 +0530

    Basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,49 @@
-import React, { useState } from 'react'
-
-
-
-
+import React from 'react'
 
 const App = () => {
-
-
-  const colors = ['bg-amber-400', 'bg-blue-400', 'bg-green-400', 'bg-red-400', 'bg-yellow-400', 'bg-pink-400', 'bg-purple-400', 'bg-indigo-400', 'bg-gray-400', 'bg-teal-400']
-  const [index, setIndex] = useState(0)
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className={`flex w-screen h-screen justify-center items-center ${colors[index]}`}>
-      <div className="flex flex-col items-center">
-      <h1 className='font-bold text-black'>Count: {count}</h1>
-      <button className='mt-5' onClick={()=> {
-          
-          if(count < 10){
-            setCount(count + 1)
-            setIndex(count+1)
-          } else {
-            setCount(0)
-            setIndex(0)
-          }
-
-
-      } 
-      }>Tap here</button>
-      </div>
+   <>
+   <div className=' mt-10 flex w-screen h-screen justify-center items-top'>
+    <div className="col">
+    <h1 className='text-center font-bold'>KTodo App</h1>
+    <div className='mt-5'>
+      <ul >
+        <li className='m-2'>
+          <h2>
+          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
+          <span className='ml-5 text-xl font-semibold'>Do exercise</span>
+          </h2></li>
+          <li className='m-2'>
+          <h2>
+          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
+          <span className='ml-5 text-xl font-semibold'>Shower</span>
+          </h2></li>
+          <li className='m-2'>
+          <h2>
+          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
+          <span className='ml-5 text-xl font-semibold'>Get rickshaw</span>
+          </h2></li>
+          <li className='m-2'>
+          <h2>
+          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
+          <span className='ml-5 text-xl font-semibold'>Rech Metro</span>
+          </h2></li>
+          <li className='m-2'>
+          <h2>
+          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
+          <span className='ml-5 text-xl font-semibold'>Use biometric</span>
+          </h2></li>
+      </ul>
     </div>
-    </>
+    <div>
+      <input className='border rounded-md mt-10 p-2' type="text" placeholder='Enter task' />
+      <button className='ml-5 text-white p-2'>Add</button>
+    </div>
+    </div>
+    </div>
+
+
+   </>
   )
 }
 
