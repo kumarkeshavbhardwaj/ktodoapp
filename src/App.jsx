8824dcb19c935219cc6f224b34853ba2f73e1e49
kumar <kumarkeshavbhardwaj@gmail.com>
2025-03-16 22:08:21 +0530

    Added Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
 
 const App = () => {
+
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  )
+
+  const handleAddTask = () => {
+    if(task){
+      setTasks([...tasks, task])
+      setTask('')
+      localStorage.setItem('tasks', JSON.stringify([...tasks, task]))
+    }
+  }
+
+
+
+
+
   return (
    <>
    <div className=' mt-10 flex w-screen h-screen justify-center items-top'>
     <div className="col">
     <h1 className='text-center font-bold'>KTodo App</h1>
     <div className='mt-5'>
-      <ul >
-        <li className='m-2'>
+      <ul className='max-h-80
+      f overflow-y-auto'>
+        {tasks.map((task, index) => (
+          <li key={index} className='m-2'>
           <h2>
           <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
-          <span className='ml-5 text-xl font-semibold'>Do exercise</span>
+          <span className='ml-5 text-xl font-semibold'>{task}</span>
           </h2></li>
-          <li className='m-2'>
-          <h2>
-          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
-          <span className='ml-5 text-xl font-semibold'>Shower</span>
-          </h2></li>
-          <li className='m-2'>
-          <h2>
-          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
-          <span className='ml-5 text-xl font-semibold'>Get rickshaw</span>
-          </h2></li>
-          <li className='m-2'>
-          <h2>
-          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
-          <span className='ml-5 text-xl font-semibold'>Rech Metro</span>
-          </h2></li>
-          <li className='m-2'>
-          <h2>
-          <input className='form-checkbox h-4 w-4 text-blue-600' type="checkbox" />
-          <span className='ml-5 text-xl font-semibold'>Use biometric</span>
-          </h2></li>
+        ))} 
+
       </ul>
     </div>
     <div>
-      <input className='border rounded-md mt-10 p-2' type="text" placeholder='Enter task' />
-      <button className='ml-5 text-white p-2'>Add</button>
+      <input className='border rounded-md mt-10 p-2' type="text" value={task} placeholder='Enter task' onChange={(e)=>{
+        setTask(e.target.value)
+      }} />
+      <button className='ml-5 text-white p-2' onClick={(e)=>{
+       handleAddTask()
+      }}>Add</button>
     </div>
     </div>
     </div>
